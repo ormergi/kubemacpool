88f8f2c58ded4bd4882ccd0de40df1e20c0d3b45
Or Mergi <ormergi@redhat.com>
2020-04-30 12:13:00 +0300

    Update multus-cni new logging usage
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- pkg/controller/pod/pod_controller.go
+++ pkg/controller/pod/pod_controller.go
@@ -82,7 +82,7 @@ func (r *ReconcilePolicy) Reconcile(request reconcile.Request) (reconcile.Result
 		if errors.IsNotFound(err) {
 			err := r.poolManager.ReleasePodMac(fmt.Sprintf("%s/%s", request.Namespace, request.Name))
 			if err != nil {
-				logging.Printf(logging.ErrorLevel, "failed to release mac for pod %s: %v", request.NamespacedName, err)
+				logging.Errorf("%v, failed to release mac for pod %s: %v",logging.ErrorLevel, request.NamespacedName, err)
 			}
 			return reconcile.Result{}, nil
 		}
